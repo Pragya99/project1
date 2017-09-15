da6f08d5fe9f6b4bfe0d540f96d539711bb25944
tiwaripragya93@gmail.com <tiwaripragya93@gmail.com>
2017-09-16 00:44:28 +0530

    Main file

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
-print "Hello world!!"
-print ' Python '
-print ' my first program'
+from Spy_details import Spy
+#spy = ['Pragya','Ms',20,3.0,True]
+question = raw_input("Do you want to continue as Pragya " +  "Enter y if Yes and n if No : ")
+if question == 'y' or question == 'Y':
+    name = raw_input("Enter your name: ")
+    print name
