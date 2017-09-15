01b637e766e561372779d695a82c1c24025b4e01
tiwaripragya93@gmail.com <tiwaripragya93@gmail.com>
2017-09-16 00:47:05 +0530

    Spy file

---
+++ Spy_details.py
@@ -0,0 +1,7 @@
+class Spy:
+    def __init__(self,name,salutation,age,rating,is_Online):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.is_Online = True
