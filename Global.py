c536c2733f6aac74290620f86be9d145edba51e7
tiwaripragya93@gmail.com <tiwaripragya93@gmail.com>
2017-09-17 11:23:47 +0530

    Global file

---
+++ Global.py
@@ -0,0 +1,5 @@
+# global variables and constants here.
+# current status message is initialized to None.
+current_status_message = None
+# list to store status messages.
+STATUS_MESSAGES = ['My name is tiwari, Pragya tiwari.', 'I believe in me only.', 'Only me, Sir!']
