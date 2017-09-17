2c6516f537e7dd3525c41b7013db3a3632941bb7
tiwaripragya93@gmail.com <tiwaripragya93@gmail.com>
2017-09-17 15:25:44 +0530

    friend file

--- Add_friend.py
+++ Add_friend.py
@@ -1,32 +1,51 @@
-# import statements.
-from globals import friends
+# importing spy and existing friends
+from spy_details import Spy, friends
+# importing regular expressions for proper validation
+import re
 
-# add new friends.
+# importing termcolor for colorful output
+from termcolor import colored
+
+# function to add new friends.
 def add_friend():
-    new_friend = {
-        'name': '',
-        'salutation': '.',
-        'age': 0,
-        'rating': 0.0,
-        'is_online': False,
-        'chats': []
-    }
-     new_friend['name'] = raw_input("Please add your friend's name: ")
-    new_friend['salutation'] = raw_input("Are they Mr. or Ms.?: ")
+# using class spy
+    new_friend = Spy(" ", " ", 0, 0.0)
 
-# concatination.
-   new_friend['name'] = new_friend['salutation'] + " " + new_friend['name']
+# ask user for name and salutation of friend
+    new_friend.name = raw_input("Please add your friend's name: ")
+     pattern_n = '^[a-zA-Z\s]+s'
 
-   new_friend['age'] = int(raw_input("Age? "))
+    new_friend.salutation= raw_input("What to call Mr. or Ms.?: ")
+    pattern_s = '^[a-zA-Z\s]+s'
+# concatination for full name
+    new_friend.name = new_friend.salutation + " " + new_friend.name
 
-   new_friend['rating'] = float(raw_input("Spy rating? "))
+# ask for age of friend, int represents type casting in int
+    new_friend.age = int(raw_input("Age? "))
+    pattern_a = '^[0-9]+$'
+
+#ask for rating of friend, float represents type casting in float
+    new_friend.rating = float(raw_input("Spy rating? "))
+    pattern_r = '^[0-9]+\.[0-9]+$'
 
 # users input validations
-if len(new_friend['name']) > 0 and new_friend['age'] > 12 and new_friend['age'] < 50:
-    friends.append(new_friend)
-    print 'Friend Added!'
-    else:
-    print 'Sorry! Invalid entry. We can\'t add spy with the details you provided'
+    if len(new_friend.name) > 0 and new_friend.name.isdigit() == False and re.match(pattern_n,new_friend.name)!=None and re.match(pattern_s,new
 
-    # returning total no of friends.
-     return len(friends)
+# add friend if conditions satisfies
+     friends.append(new_friend)
+     print (colored('Friend Added!', 'magenta'))
+     else:
+    print (colored('Sorry! Invalid entry. We can\'t add spy with the details you provided\n ', 'blue'))
+print (colored('The convention to follow is: \n ', 'blue'))
+print (colored('1. Name can be alphabets only.\n ', 'blue'))
+print (colored('2. Age can be digits only.\n ', 'blue'))
+print (colored('3. Do not use (.) after salutation, it can be alphabets only too.\n ', 'blue'))
+print (colored('Keep in mind and Try Again\n\n ', 'blue'))
+
+# returning total no of friends.
+return len(friends)
+
+
+
+
+
